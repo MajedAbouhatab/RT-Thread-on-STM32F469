d052587501943362f6c8e880ca0ebb80214c9c3a
Majed Abouhatab, P.E <45211996+MajedAbouhatab@users.noreply.github.com>
2022-08-14 09:13:23 -0500

    Update and rename lv_conf.h to Modified Files/lv_conf.h

---
+++ Modified Files/lv_conf.h
@@ -0,0 +1,11 @@
+#ifndef LV_CONF_H
+#define LV_CONF_H
+
+#define LV_HOR_RES_MAX          800
+#define LV_VER_RES_MAX          480
+
+#define LV_FONT_MONTSERRAT_38   1
+#define LV_FONT_MONTSERRAT_48   1
+#define LV_FONT_DEFAULT &lv_font_montserrat_38
+
+#endif
